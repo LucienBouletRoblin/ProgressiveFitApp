5c16fb6b9e5e30c853d7b3d9d27878db463142e8
Lucien Boulet-Roblin <lucien.boulet-roblin@capgemini.com>
2018-08-19 20:00:57 +0200

    fix removing redux offline dependencie

--- progressive-fit-app/src/store/configureStore.js
+++ progressive-fit-app/src/store/configureStore.js
@@ -1,12 +1,10 @@
 import { applyMiddleware, createStore } from "redux";
 import { composeWithDevTools } from "redux-devtools-extension";
-import offlineConfig from "@redux-offline/redux-offline/lib/defaults";
-import { offline } from "@redux-offline/redux-offline";
 import thunk from "redux-thunk";
 
 import reducers from "../reducers";
 
 export default createStore(
   reducers,
-  composeWithDevTools(applyMiddleware(thunk), offline(offlineConfig))
+  composeWithDevTools(applyMiddleware(thunk))
 );
